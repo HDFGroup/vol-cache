768296c60c010511e7416c9df396186771ffcf42
Huihuo Zheng <huihuo.zheng@anl.gov>
2021-03-16 12:40:57 -0500

    added vpicio tests

---
+++ tests/vpicio_hdf5/timer.h
@@ -0,0 +1,81 @@
+/****** Copyright Notice ***
+ *
+ * PIOK - Parallel I/O Kernels - VPIC-IO, VORPAL-IO, and GCRM-IO, Copyright
+ * (c) 2015, The Regents of the University of California, through Lawrence
+ * Berkeley National Laboratory (subject to receipt of any required
+ * approvals from the U.S. Dept. of Energy).  All rights reserved.
+ *
+ * If you have questions about your rights to use or distribute this
+ * software, please contact Berkeley Lab's Innovation & Partnerships Office
+ * at  IPO@lbl.gov.
+ *
+ * NOTICE.  This Software was developed under funding from the U.S.
+ * Department of Energy and the U.S. Government consequently retains
+ * certain rights. As such, the U.S. Government has been granted for itself
+ * and others acting on its behalf a paid-up, nonexclusive, irrevocable,
+ * worldwide license in the Software to reproduce, distribute copies to the
+ * public, prepare derivative works, and perform publicly and display
+ * publicly, and to permit other to do so.
+ *
+ ****************************/
+
+/**
+ *
+ * Email questions to SByna@lbl.gov
+ * Scientific Data Management Research Group
+ * Lawrence Berkeley National Laboratory
+ *
+*/
+
+#ifndef __SSI_TIMER_H__
+#define __SSI_TIMER_H__
+
+#include <sys/time.h>
+
+#define DTYPE float
+
+extern struct timeval start_time[10];
+extern float elapse[10];
+#define timer_on(id) gettimeofday (&start_time[id], NULL)
+#define timer_off(id) 	\
+		{	\
+		     struct timeval result, now; \
+		     gettimeofday (&now, NULL);  \
+		     timeval_subtract(&result, &now, &start_time[id]);	\
+		     elapse[id] += result.tv_sec+ (DTYPE) (result.tv_usec)/1000000.;	\
+		}
+
+#define timer_msg(id, msg) \
+	printf("%f seconds elapsed in %s\n", (DTYPE)(elapse[id]), msg);  \
+
+#define timer_reset(id) elapse[id] = 0
+
+/* Subtract the `struct timeval' values X and Y,
+   storing the result in RESULT.
+   Return 1 if the difference is negative, otherwise 0.  */
+
+static inline int
+timeval_subtract (struct timeval *result, struct timeval *x, struct timeval *y)
+{
+  /* Perform the carry for the later subtraction by updating y. */
+  if (x->tv_usec < y->tv_usec) {
+    int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
+    y->tv_usec -= 1000000 * nsec;
+    y->tv_sec += nsec;
+  }
+  if (x->tv_usec - y->tv_usec > 1000000) {
+    int nsec = (y->tv_usec - x->tv_usec) / 1000000;
+    y->tv_usec += 1000000 * nsec;
+    y->tv_sec -= nsec;
+  }
+
+  /* Compute the time remaining to wait.
+     tv_usec is certainly positive. */
+  result->tv_sec = x->tv_sec - y->tv_sec;
+  result->tv_usec = x->tv_usec - y->tv_usec;
+
+  /* Return 1 if result is negative. */
+  return x->tv_sec < y->tv_sec;
+}
+
+#endif
